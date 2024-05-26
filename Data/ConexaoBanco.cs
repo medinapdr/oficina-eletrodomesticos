8af1f55d7957a833a9958e55a3ac463676e9ebb5
medinapdr <m3dina@protonmail.com>
2024-05-26 11:38:59 -0300

    Criar app.config

--- Data/ConexaoBanco.cs
+++ Data/ConexaoBanco.cs
@@ -1,19 +1,15 @@
-﻿using System.Data.SqlClient;
+﻿using System.Configuration;
+using System.Data.SqlClient;
 
 namespace OficinaEletrodomesticos.Data
 {
     public class ConexaoBanco
     {
-        private readonly string _connectionString;
-
-        public ConexaoBanco()
-        {
-            _connectionString = "Server=tcp:oficinaeletro.database.windows.net,1433;Initial Catalog=Oficina;Persist Security Info=False;User ID=oficinator;Password=DCC603@admin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        }
-
         public SqlConnection ConectaBanco()
         {
-            return new SqlConnection(_connectionString);
+            string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            return new SqlConnection(connectionString);
         }
     }
 }
