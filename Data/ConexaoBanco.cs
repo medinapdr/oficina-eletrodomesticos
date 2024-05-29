bab25bcc1b96424939b872f2cb7374dee0f977a1
medinapdr <m3dina@protonmail.com>
2024-05-29 01:27:19 -0300

    Criar Testes

--- Data/ConexaoBanco.cs
+++ Data/ConexaoBanco.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 
 namespace OficinaEletrodomesticos.Data
 {
@@ -7,7 +6,7 @@ namespace OficinaEletrodomesticos.Data
     {
         public static SqlConnection ConectaBanco()
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            string connectionString = "Server=tcp:oficinaeletro.database.windows.net,1433;Initial Catalog=Oficina;Persist Security Info=False;User ID=oficinator;Password=DCC603@admin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
             return new SqlConnection(connectionString);
         }
